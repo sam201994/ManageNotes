eb5f0d8ec1e823315fcc2adcc9602f7b2331c53b
Smriti <smriti@Smritis-MacBook-Pro.local>
2017-05-02 13:38:46 +0530

    updated webpack to ato add css

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,51 @@ var webpack = require('webpack');
 var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'client/dist');
 var APP_DIR = path.resolve(__dirname, 'client/src');
-console.log("herehrerere");
-const config = {
+
+var config = {
   entry: APP_DIR + '/index.jsx',
+  module: {
+    rules: [
+      {
+        include: APP_DIR,
+        exclude: /(node_modules|bower_components)/,
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-         presets: ['es2015', 'react', 'stage-2']
-        }
-      }
-    ]
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
 
